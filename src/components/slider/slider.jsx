bf2ed103f36d7c9692da27a69daf04384b5f8646
unknown <serega.mironov11@gmail.com>
2021-11-23 17:45:30 +0300

    Добавил слайдер(WIP)

---
+++ src/components/slider/slider.jsx
@@ -0,0 +1,38 @@
+import {useState} from "react";
+import sliderStyle from "./slider.module.css";
+
+const Slider = ({className, children = []}) => {
+  const [currentSlideIndex, setCurrentSlideIndex] = useState(0);
+
+  const checkIndex = (index) => {
+    return Array.isArray(children) & children.hasOwnProperty(index) ? true : false;
+  };
+
+  return (
+    <div className={`${sliderStyle.Slider} ${className}`}>
+      <div className={sliderStyle.wrapper}>
+        <div className={sliderStyle.slide}>
+          {Array.isArray(children) ? children[currentSlideIndex] : children}
+        </div>
+      </div>
+      <div className={sliderStyle.controls}>
+        <button
+          className={sliderStyle.controlPrev}
+          type="button"
+          aria-label="Previous slide"
+          disabled = {checkIndex(currentSlideIndex - 1) ? false : true}
+          onClick={() => setCurrentSlideIndex(currentSlideIndex - 1)}
+        >prev</button>
+        <button
+          className={sliderStyle.controlNext}
+          type="button"
+          aria-label="Next slide"
+          disabled = {checkIndex(currentSlideIndex + 1) ? false : true}
+          onClick={() => setCurrentSlideIndex(currentSlideIndex + 1)}
+        >next</button>
+      </div>
+    </div>
+  );
+};
+
+export default Slider;
