1d455e8cdf77c6bef817f132110278c1826783a8
unknown <serega.mironov11@gmail.com>
2021-12-02 11:07:39 +0300

    Добавил бд mondoDB(WIP)

--- src/pages/api/subscribe.js
+++ src/pages/api/subscribe.js
@@ -1,8 +1,24 @@
-const handler = (req, res) => {
+const mongoose = require(`mongoose`);
+
+// INSERT PASS FROM PASS STORE
+const CONNECT_MONGO_URL = `PASSWORD`;
+
+const handler = async (req, res) => {
   let queries = {};
 
   if (req.method === `POST`) {
     queries = req.body;
+    await mongoose.connect(CONNECT_MONGO_URL);
+
+    const Feedback = await mongoose.model(`feedbacks`);
+    await Feedback.insertMany({
+      name: queries.name,
+      contacts: queries.contacts,
+      message: queries.message,
+    });
+
+    await mongoose.disconnect();
+
     res.status(200).send(`okey`);
     console.log(queries);
   }
